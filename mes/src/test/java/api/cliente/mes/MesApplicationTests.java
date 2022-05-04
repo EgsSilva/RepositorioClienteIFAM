1a900f0ca9e2a1854738ab563e15de67835705bc
EgsSilva <2019003118@ifam.edu.br>
2022-05-04 13:48:40 -0400

    [chore]: create base app.

---
+++ mes/src/test/java/api/cliente/mes/MesApplicationTests.java
@@ -0,0 +1,13 @@
+package api.cliente.mes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
